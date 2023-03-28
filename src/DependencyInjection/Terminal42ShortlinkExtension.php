c418aad0a0c0a1c0a73e098cddd9da850ee0d4e9
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-03-28 10:05:24 +0200

    Apply config in extension instead of with parameters

--- src/DependencyInjection/Terminal42ShortlinkExtension.php
+++ src/DependencyInjection/Terminal42ShortlinkExtension.php
@@ -8,12 +8,11 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Terminal42\ShortlinkBundle\Controller\ShortlinkController;
+use Terminal42\ShortlinkBundle\ShortlinkGenerator;
 
 class Terminal42ShortlinkExtension extends ConfigurableExtension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function loadInternal(array $config, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
@@ -23,8 +22,25 @@ class Terminal42ShortlinkExtension extends ConfigurableExtension
 
         $loader->load('services.yml');
 
-        $container->setParameter('terminal42_shortlink.host', $config['host']);
-        $container->setParameter('terminal42_shortlink.salt', $config['salt']);
-        $container->setParameter('terminal42_shortlink.log_ip', $config['log_ip']);
+        $container
+            ->getDefinition(ShortlinkController::class)
+            ->setArgument(2, (bool) $config['log_ip'])
+        ;
+
+        $container
+            ->getDefinition(ShortlinkGenerator::class)
+            ->setArgument(2, $config['host'])
+        ;
+
+        $container
+            ->getDefinition('terminal42_shortlink.hashids')
+            ->setArgument(0, $config['salt'])
+        ;
+
+        $container
+            ->getDefinition('terminal42_shortlink.routing.route_provider')
+            ->setArgument(2, $config['host'])
+            ->setArgument(3, $config['catchall_redirect'])
+        ;
     }
 }
